700af0dc3ba7be32dafb6bba8dee45e7c9acd69d
박상민 <iamsangminpark@gmail.com>
2021-06-22 19:25:25 +0900

    typing practice

--- learn-typescript/quiz/1_todo/src/index.ts
+++ learn-typescript/quiz/1_todo/src/index.ts
@@ -1,7 +1,7 @@
-let todoItems: any;
+let todoItems: {id: number; title: string; done: boolean}[];
 
 // api
-function fetchTodoItems(): object {
+function fetchTodoItems(): {id: number; title: string; done: boolean}[] {
   const todos = [
     { id: 1, title: '안녕', done: false },
     { id: 2, title: '타입', done: false },
@@ -11,12 +11,12 @@ function fetchTodoItems(): object {
 }
 
 // crud methods
-function fetchTodos(): object {
+function fetchTodos(): object[] {
   const todos = fetchTodoItems();
   return todos;
 }
 
-function addTodo(todo: object): void {
+function addTodo(todo: {id: number; title: string; done: boolean}): void {
   todoItems.push(todo);
 }
 
@@ -24,33 +24,31 @@ function deleteTodo(index: number): void {
   todoItems.splice(index, 1);
 }
 
-function completeTodo(index: number, todo: any) {
+function completeTodo(index: number, todo: {id: number; title: string; done: boolean}): void {
   todo.done = true;
   todoItems.splice(index, 1, todo);
 }
-// todo 모르겠음 ;;
 
 // business logic
-function logFirstTodo() {
+function logFirstTodo():object {
   return todoItems[0];
 }
 
-function showCompleted() {
-  return todoItems.filter((item:any) => item.done);
-  // 완료된 것만 화면에 보여주기
-  // item도 모르겠다.
+function showCompleted(): {id: number; title: string; done: boolean}[] {
+  return todoItems.filter((item: object) => item.done);
 }
 
 // TODO: 아래 함수의 내용을 채워보세요. 아래 함수는 `addTodo()` 함수를 이용하여 2개의 새 할 일을 추가하는 함수입니다.
-function addTwoTodoItems() {
+function addTwoTodoItems(): void {
   // addTodo() 함수를 두 번 호출하여 todoItems에 새 할 일이 2개 추가되어야 합니다.
-  for (i=0; i<2; i++) {
-    addTodo();
+  const item1 = {
+    id: 4, title: 'item4', done: false,
   }
+  addTodo(item1);
 }
 
 // NOTE: 유틸 함수
-function log() {
+function log(): void {
   console.log(todoItems);
 }
 
