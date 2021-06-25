aacfafd40860604107dd44585f3842f2afc9222e
박상민 <iamsangminpark@gmail.com>
2021-06-25 15:54:45 +0900

    apply module

--- learn-typescript/quiz/2_address-book/src/index-ans.ts
+++ learn-typescript/quiz/2_address-book/src/index-ans.ts
@@ -1,20 +1,4 @@
-interface PhoneNumberDictionary {
-    [phone: string]: {
-      num: number;
-    };
-  }
-
-interface Contact {
-    name: string;
-    address: string;
-    phones: PhoneNumberDictionary;
-}
-
-enum PhoneType {
-  Home = 'home',
-  Office = 'office',
-  Studio = 'studio',
-}
+import { Contact, PhoneType } from './index-type';
 
 // api
 // TODO: 아래 함수의 반환 타입을 지정해보세요.
