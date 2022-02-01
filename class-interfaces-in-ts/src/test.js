7006b4573774fa0f90f13cb7be38b689bb0ad679
steadily-worked <iamsangminpark@gmail.com>
2022-02-01 13:26:24 +0900

    delete unnecessary files

--- class-interfaces-in-ts/src/test.js
+++
@@ -1,8 +0,0 @@
-var states = [
-    { name: "Korea", capital: "Seoul" },
-    { name: "Japan", capital: "Tokyo" },
-];
-for (var _i = 0, states_1 = states; _i < states_1.length; _i++) {
-    var state = states_1[_i];
-    console.log(state.capital);
-}
