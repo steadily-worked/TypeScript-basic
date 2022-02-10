da12b7796c76915cbb5376250bf2fa1992775e3f
steadily-worked <iamsangminpark@gmail.com>
2022-02-10 12:45:37 +0900

    apply Webpack

--- webpack-with-typescript/src/app.ts
+++ webpack-with-typescript/src/app.ts
@@ -1,6 +1,6 @@
-import { ProjectInput } from './components/project-input';
-import { ProjectList } from './components/project-list';
+import { ProjectInput } from "./components/project-input";
+import { ProjectList } from "./components/project-list";
 
 new ProjectInput();
-new ProjectList('active');
-new ProjectList('finished');
+new ProjectList("active");
+new ProjectList("finished");
--- webpack-with-typescript/webpack.config.prod.js
+++ webpack-with-typescript/webpack.config.prod.js
@@ -1,8 +1,10 @@
 const path = require("path");
 // webpack dev server 모드에서, bundle은 메모리에서만 생성된다.
 // 따라서 dist에 쓰여지지 않고, 이에 dist 폴더에 내용물이 없어서는 안되지는 않는다.
+const CleanPlugin = require("clean-webpack-plugin");
+
 module.exports = {
-  mode: "development",
+  mode: "production",
   entry: "./src/app.ts",
   output: {
     filename: "bundle.js",
@@ -11,7 +13,8 @@ module.exports = {
     // tsconfig.json의 outDir 경로와 맞춰야 함
     publicPath: "dist",
   },
-  devtool: "inline-source-map",
+  devtool: "none",
+  // 프로덕션 환경에서는 devtool이 필요없음
   module: {
     rules: [
       // 웹팩이 모든 파일에 적용될 규칙을 설정
@@ -26,4 +29,10 @@ module.exports = {
   resolve: {
     extensions: [".ts", ".js"], // 찾아야 할 파일
   },
+  plugins: [
+    // general workflow에 적용
+    // 새로운 출력이 여기 쓰이기 전에 dist 폴더 내의 모든것을 삭제하는 플러그인을 추가해야 함
+    new CleanPlugin.CleanWebpackPlugin(),
+    // 이렇게 해서 만든 dist의 bundle.js는 프로덕션을 위해 최적화된 코드이다.
+  ],
 };
